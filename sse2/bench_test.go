d90f5645b1de76641afe2660ee2d419611728414
zhuo.meng <mengzhuo1203@gmail.com>
2017-06-22 22:59:50 +0800

    change bench_test

--- sse2/bench_test.go
+++ sse2/bench_test.go
@@ -2,36 +2,6 @@ package sse2
 
 import "testing"
 
-func TestPMINUBByte(t *testing.T) {
-	a := []byte{1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2}
-	b := []byte{2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1}
-	PMINUBm128byte(a, b)
-	t.Log(a, b)
-}
-
-func TestPMADDWDint32(t *testing.T) {
-	a := []int32{2, 3, 4, 5}
-	b := []int32{2, 3, 4, 5}
-	PMADDWDm128int32(a, b)
-	t.Log(a, b)
-}
-
-func TestPSUBDint32(t *testing.T) {
-	a := []int32{3, 3, 5, 5}
-	b := []int32{2, 3, 4, 5}
-	PSUBDm128int32(a, b)
-	t.Log(a, b)
-}
-
-/*
-func TestSHUFPD(t *testing.T) {
-	a := []float32{3, 3, 5, 5}
-	b := []float32{2, 3, 4, 5}
-	SHUFPDm128float32(a, b, 1)
-	t.Log(a, b)
-}
-*/
-
 func generalMin(a, b []byte) {
 	for i := 0; i < len(a); i++ {
 		if a[i] < b[i] {
